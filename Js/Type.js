4734a74f7b5a22812be4e22cc186b6f5148d82cf
Drastick17 <85968388+Drastick17@users.noreply.github.com>
2021-09-02 15:23:13 -0500

    Add files via upload

---
+++ Js/Type.js
@@ -0,0 +1,9 @@
+const typed = new Typed('.typed',{
+    strings:['Web','Python','Fullstack','Software'],
+    typeSpeed:75,
+    startDelay:300,
+    backDelay:1500,
+    backSpeed:75,
+    shuffle:true,
+    loop:true
+  });
